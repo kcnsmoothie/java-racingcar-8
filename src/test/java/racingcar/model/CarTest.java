ee85be05801b83fae726212a57dfdfc76d47eb6e
kcnsmoothie <kcnsmoothie05@gmail.com>
2025-10-26 22:58:12 +0900

    test: 자동차 이름 유효성 검사 테스트 추가

---
+++ src/test/java/racingcar/model/CarTest.java
@@ -0,0 +1,28 @@
+package racingcar.model;
+
+import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class CarTest {
+
+    @ParameterizedTest
+    @DisplayName("입력값이 null 혹은 공백인지 검사")
+    @NullAndEmptySource
+    void nameNullOrEmpty(String carNames) {
+        assertThatThrownBy(() -> new Car(carNames))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @DisplayName("자동차 이름이 5자 초과인지 검사하는 테스트")
+    @ValueSource(strings = "pobi,errorname")
+    void nameLengthTest(String carNames) {
+        assertThatThrownBy(() -> new Car(carNames))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
