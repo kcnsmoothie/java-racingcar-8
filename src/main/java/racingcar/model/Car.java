38aa25026398f2f39e54a0cc8ed6c80da999c9ee
kcnsmoothie <kcnsmoothie05@gmail.com>
2025-12-25 16:37:33 +0900

    feat: 자동차 객체 생성 기능 구현

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,4 +1,19 @@
 package racingcar.model;
 
 public class Car {
+    private final String name;
+    private final int distance;
+
+    public Car(String name, int distance) {
+        this.name = name;
+        this.distance = distance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
 }
